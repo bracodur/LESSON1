a5c851c410d70e2fff51ef9929c503d2a9dbb18e
Jane Turova <turova@mail.ru>
2024-01-26 11:23:17 +0700

    add some variables meet_lang/urok1

--- urok1/Program.cs
+++ urok1/Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("Hello world");
+﻿//Console.WriteLine("Hello world");
 
+int value = 100;
+Console.WriteLine(value);
+double d = 5.55;
+Console.WriteLine(d);
+char c = '!';
+Console.WriteLine(c);
+bool b = true;
+Console.WriteLine(b);
