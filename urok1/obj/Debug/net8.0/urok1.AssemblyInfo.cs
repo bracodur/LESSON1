ac15480880f5b5229a28f394a372a2eed13b4ee4
Jane Turova <turova@mail.ru>
2024-01-26 10:50:23 +0700

    add meet_lang/urok1

--- urok1/obj/Debug/net8.0/urok1.AssemblyInfo.cs
+++ urok1/obj/Debug/net8.0/urok1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("urok1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61dbc17b7884681c76547ebc815648b464cdb6c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("urok1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("urok1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
